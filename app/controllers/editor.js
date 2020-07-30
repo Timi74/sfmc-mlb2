2f81e1aa5898ac6908b1e8ce4e8df32bc1d404b4
Yannick THIMEL <yannick.thimel@amersports.com>
2020-07-30 17:09:07 +0200

    YTH: init default MLB2 config

---
+++ app/controllers/editor.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const sfmc = require('sfmc-nodesdk');
+const path = require('path');
+const utils = requireRoot('modules/utils');
+const db = requireRoot('modules/db');
+const editorApi = requireRoot('modules/editor-api');
+const router = express.Router();
+
+/* Workaround for the block icon issue */
+router.get(/.*(drag)?icon\.png$/i, async function (req, res) {
+	let iconFile = path.parse(req.originalUrl).base;
+
+	res.redirect('/public/img/' + iconFile);
+});
+
+router.get('/:package([-\\w]+)/', async function (req, res, next) {
+	try {
+		res.redirect('/public/html/editor.html');
+		return;
+	} catch (err) {
+		next({
+			type: 'MLB_NOT_INITIALIZED',
+			details: err
+		});
+	}
+});
+
+module.exports = router;
