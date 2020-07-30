c96f5080c37e07a56d5742fe24bd4a25ac51d733
Yannick THIMEL <yannick.thimel@amersports.com>
2020-07-30 23:46:20 +0200

    YTH: init default MLB2 config

--- app/app.js
+++ app/app.js
@@ -1,10 +1,24 @@
 var config		= requireRoot('config');
 var express 	= require('express');
 var app 		= express();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'pug');
 app.set('views', rootPath + '/app/views');
 
+app.use('/public', express.static('public'));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.use(function (err, req, res, next) {
+	if (err) {
+		res.send(JSON.stringify(err, Object.getOwnPropertyNames(err), 2));
+		res.end();
+	} else {
+		next();
+	}
+})
 
 /* Declare controllers */
 app.use('/editor', requireRoot('controllers/editor'));
