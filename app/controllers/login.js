1be9faba97246cc41771be31464a55e0ef0bcd18
Yannick THIMEL <yannick.thimel@amersports.com>
2020-08-03 16:54:22 +0200

    YTH: implement new OAuth2 token management

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/authorize/:package([-\\w]+)/', async function (req, res, next) {
 	try {
-		
+		console.log('login --> authorization: ' + (new Date()));
 		res.redirect('https://mct5rqj2m5hmqwlh05v-qstxmh9m.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id=ptfdr87gnrp5z662mpytawqc&redirect_uri=https%3A%2F%2Fsfmc-amer.herokuapp.com%2Feditor%2F5647b4f8-909d-4d8f-8a5d-19940149761d%2F&scope=email_read%20email_write%20data_extensions_read%20data_extensions_write&state=mlb_editor');
 		return;
 	} catch (err) {
@@ -20,11 +20,12 @@ router.get('/authorize/:package([-\\w]+)/', async function (req, res, next) {
 
 router.get('/token/:package([-\\w]+)/', async function (req, res, next) {
 	try {
-		
+		console.log('login --> get token: ' + (new Date()));
 		var payload = {
 			"grant_type":		"authorization_code",
 			"code":				req.query.code,
 			"client_id":        "ptfdr87gnrp5z662mpytawqc",
+			"client_secret":    "ngSrW0ZkKTaQ0sj6LLVtO6DV",
 			"redirect_uri":     "https://sfmc-amer.herokupp.com/editor/5647b4f8-909d-4d8f-8a5d-19940149761d/",
 			"scope":			"email_read email_write data_extensions_read data_extensions_write"
 		};
@@ -39,7 +40,7 @@ router.get('/token/:package([-\\w]+)/', async function (req, res, next) {
 		);
 
 		var result = JSON.parse(response);
-		
+		console.log(result);
 		res.redirect('/public/html/editor.html');
 		return;
 	} catch (err) {
