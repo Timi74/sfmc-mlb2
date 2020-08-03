feebd2147d20586eb4abcce9285e2d2fe592cf36
Yannick THIMEL <yannick.thimel@amersports.com>
2020-08-03 09:46:47 +0200

    YTH: implement new OAuth2 token management

--- app/app.js
+++ app/app.js
@@ -5,6 +5,8 @@ const app 		= express();
 app.set('view engine', 'pug');
 app.set('views', rootPath + '/app/views');
 
+app.use('/public', express.static('public'));
+
 /* Declare controllers */
 app.use('/editor', requireRoot('controllers/editor'));
 
