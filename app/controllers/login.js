38746a752932f8c8bcca546de8e255913b93a672
Yannick THIMEL <yannick.thimel@amersports.com>
2020-08-03 12:04:15 +0200

    YTH: implement new OAuth2 token management

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -8,10 +8,7 @@ const router = express.Router();
 router.get('/:package([-\\w]+)/', async function (req, res, next) {
 	try {
 		
-		var response = await sfmc.httpLayer.executeHttpCall({uri:"https://mct5rqj2m5hmqwlh05v-qstxmh9m.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id=pz1titr5p832x7uxqywrkr8v&redirect_uri=https%3A%2F%2Fsfmc-amer.herokuapp.com%2Feditor%2F36f68e83-28dd-4c8c-b58e-4b9bdd12dced%2F&scope=email_read%20email_write%20data_extensions_read%20data_extensions_write&state=mlb_editor", 
-		method:"POST"});
-		);
-		res.redirect('/public/html/editor.html');
+		res.redirect('https://mct5rqj2m5hmqwlh05v-qstxmh9m.auth.marketingcloudapis.com/v2/authorize?response_type=code&client_id=ptfdr87gnrp5z662mpytawqc&redirect_uri=https%3A%2F%2Fsfmc-amer.herokuapp.com%2Feditor%2F5647b4f8-909d-4d8f-8a5d-19940149761d%2F&scope=email_read%20email_write%20data_extensions_read%20data_extensions_write&state=mlb_editor');
 		return;
 	} catch (err) {
 		next({
