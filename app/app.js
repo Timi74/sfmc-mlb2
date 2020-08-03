8a37614277f9511814c65d254fbc26586cfb5e3f
Yannick THIMEL <yannick.thimel@amersports.com>
2020-08-03 14:27:26 +0200

    YTH: implement new OAuth2 token management

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@ const config	= requireRoot('config');
 const express 	= require('express');
 const app 		= express();
 const bodyParser = require('body-parser');
+const redis = require("redis");
+const session = require('express-session');
+const client = redis.createClient(process.env.REDIS_URL);
 
 app.set('view engine', 'pug');
 app.set('views', rootPath + '/app/views');
@@ -19,6 +22,21 @@ app.use(function (err, req, res, next) {
 		next();
 	}
 })
+
+app.use(session({
+	store: new RedisStore({
+		client: client,
+		prefix: 'session_',
+		ttl: 50000
+	}),
+	secret: config.sessionSecret,
+	resave: false,
+	saveUninitialized: false,
+	ttl: 50000,
+	cookie: {
+		secure: config.sessionIsSecure
+	}
+}));
 
 /* Declare controllers */
 app.use('/editor', requireRoot('controllers/editor'));
