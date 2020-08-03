3f1d47d1fcd88ca69ada8f8ad4198392c99d7ed9
Yannick THIMEL <yannick.thimel@amersports.com>
2020-08-03 10:05:42 +0200

    YTH: implement new OAuth2 token management

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,17 +1,19 @@
-const express  = require('express');
-const config   = requireRoot('config');
-const router   = express.Router();
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
 
 router.get('/:package([-\\w]+)/', async function (req, res, next) {
 	try {
-		console.log('Init login');
+		res.redirect('/public/html/editor.html');
+		return;
 	} catch (err) {
 		next({
 			type: 'MLB_NOT_INITIALIZED',
 			details: err
 		});
 	}
-	res.end();
-}
+});
+
 
 module.exports = router;
