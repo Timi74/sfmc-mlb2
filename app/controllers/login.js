5d2d929be41e3c09eb56f1cb2919bdac284ef012
Yannick THIMEL <yannick.thimel@amersports.com>
2020-08-03 11:12:24 +0200

    YTH: implement new OAuth2 token management

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -7,8 +7,14 @@ const router = express.Router();
 
 router.get('/:package([-\\w]+)/', async function (req, res, next) {
 	try {
+		var sdk = new SDK();
+		sdk.triggerAuth2({authURL:"https://mct5rqj2m5hmqwlh05v-qstxmh9m.auth.marketingcloudapis.com/",
+		                  clientID:"pz1titr5p832x7uxqywrkr8v",
+						  redirectURL:"https://sfmc-amer.herokuapp.com/editor/36f68e83-28dd-4c8c-b58e-4b9bdd12dced/",
+						  scope:["email_read","email_write","data_extensions_read","data_extensions_write"],
+						  state: "mbl_editor"});
 		
-		res.redirect('/public/html/editor.html');
+		/*res.redirect('/public/html/editor.html');*/
 		return;
 	} catch (err) {
 		next({
