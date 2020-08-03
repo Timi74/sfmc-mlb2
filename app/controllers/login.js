bd21df6193f43d7da726eee89f553af2b5c547ad
Yannick THIMEL <yannick.thimel@amersports.com>
2020-08-03 10:50:45 +0200

    YTH: implement new OAuth2 token management

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const blocksdk = requireRoot('public/js/blocksdk');
+var blocksdk = requireRoot('module/blocksdk');
 const router = express.Router();
 
 
