100111c1834afb1d345054eb44b18dd2f1fa93a8
Yannick THIMEL <yannick.thimel@amersports.com>
2020-10-14 11:25:35 +0200

    Configure Editor API

--- app/controllers/editor.js
+++ app/controllers/editor.js
@@ -59,11 +59,16 @@ router.post('*', function (req, res, next) {
 
 router.post('/api/:action([\\w]+)', async function (req, res) {
 	/*try {*/
-		
+		sfmc.core.init({
+			token: res.locals.token,
+			soapBaseUrl: res.locals.token.soap_instance_url,
+			restBaseUrl: res.locals.token.rest_instance_url
+		});
+
 		let sfmctoken = await sfmc.core.getToken();
 		console.log("SMFC node module initialized");
 		console.log(JSON.stringify(sfmctoken));
-		
+
 		editorApi[req.params.action](req, res);
 	/*} catch (err) {
 		next({
